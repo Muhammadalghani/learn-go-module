9b74650f7569f5e4dfe82db3a2ed2fd5578b0df2
Muhammad Al Ghani <muhammadalghani2106@gmail.com>
2023-03-21 09:51:44 +0700

    update

--- slice_service.go
+++ slice_service.go
@@ -7,3 +7,22 @@ func PrintSliceOfString( slc []string){
 		fmt.Println(v)
 	}
 }
+
+// git tag v1.0.0
+// git push origin v1.0.0 
+
+// go mod tidy -> untuk mengimpor nya 
+// atau bisa juga dengan 
+// go get link_githubnya
+
+/*
+1 create repo di github (github.com/Muhammadalghani/learn-go-module)
+2. inisiasi go mod dengan domain github repository
+3. push
+4 step setelah push :
+	- create tagging 
+		- git tag v1.0.0
+		- git push origin v1.0.0
+5. consume package 
+	- go get -u github.com/Muhammadalghani/learn-go-module
+*/ 
